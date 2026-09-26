3680edbe1b9c804613219f0804db77f45d5e8454
jebalads-del <jebal.ads@gmail.com>
2026-09-26 07:04:15 +0300

    Redirect entry point directly to entrypoint.ts

--- apps/mobile/index.tsx
+++ apps/mobile/index.tsx
@@ -1,12 +1,1 @@
-import 'react-native-url-polyfill/auto';
-import '@expo/metro-runtime';
-import { registerRootComponent } from 'expo';
-import { ExpoRoot } from 'expo-router';
-
-// Must be exported or called for Expo Router to resolve correctly
-export function App() {
-  const ctx = require.context('./app');
-  return <ExpoRoot context={ctx} />;
-}
-
-registerRootComponent(App);
+import './entrypoint';
